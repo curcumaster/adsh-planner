813d0233c2ddcc9e4f2dc83cbd521754f0d8961a
stiv <stiv@db.db>
2024-08-12 03:35:26 +0300

    [Feature] Incorporate a SQL database and implement CRUD operations

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn
+
 from routes.users import user_router
 from routes.events import event_router
 import uvicorn
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 
 if __name__ == "__main__":
